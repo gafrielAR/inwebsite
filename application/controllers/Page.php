75accc9d88b7b8125991d3b1bd9b7e347c8683cb
GafrielAR <37017601+gafrielAR@users.noreply.github.com>
2021-03-25 21:05:36 +0800

    split index

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -6,17 +6,17 @@ class Page extends MY_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		// $this->data['header'] = $this->parser->parse('frontend/header_frontend.html', $this->data, true);
-		// $this->data['menu'] = $this->parser->parse('frontend/frontend_home.html', $this->data, true);
-		// $this->data['breadcrum'] = $this->parser->parse('frontend/frontend_home.html', $this->data, true);
-		// $this->data['body'] = $this->parser->parse('frontend/body_frontend.html', $this->data, true);
-		// $this->data['footer'] = $this->parser->parse('frontend/footer_frontend.html', $this->data, true);
+		$this->data['header'] = $this->parser->parse('frontend/parsial/header.html', $this->data, true);
+		// $this->data['menu'] = $this->parser->parse('frontend/parsial/menu.html', $this->data, true);
+		// $this->data['breadcrum'] = $this->parser->parse('frontend/parsial/breadcrum.html', $this->data, true);
+		// $this->data['body'] = $this->parser->parse('frontend/parsial/body.html', $this->data, true);
+		$this->data['footer'] = $this->parser->parse('frontend/parsial/footer.html', $this->data, true);
 	}
     /*****************************************************************************/
     /*****************************************************************************/
 	function index()
     {
-		// $this->data['content'] = $this->parser->parse('frontend/index.html', $this->data, true);
+		$this->data['content'] = $this->parser->parse('frontend/default/home.html', $this->data, true);
 		$this->parser->parse($this->template.'index.html', $this->data, false);
 	}
     /*****************************************************************************/
